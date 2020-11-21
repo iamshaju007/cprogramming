75fda6a2c1a15d0d0566b97ccb58578ae6635219
iamshaju007 <74230986+iamshaju007@users.noreply.github.com>
2020-11-21 10:25:56 +0530

    Add files via upload

---
+++ c progaming codes/naturalreverse.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+    int n;
+    
+printf("\nenter a ending number:");
+    
+scanf("%d",&n);
+    
+printf("\n the list of natural numbers:");
+    
+for(int i=n;i<=1;i--)
+    
+{
+        printf("%d",i);
+    }
+    
+return 0;
+
+}
---
+++ natural number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+    int n;
+    printf("\nenter a ending number:");
+    
+scanf("%d",&n);
+   
+ printf("\n the list of natural numbers:");
+    
+for(int i=1;i<=n;i++)
+    
+{
+        printf("%d",i);
+    }
+    
+return 0;
+}
